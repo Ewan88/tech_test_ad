59731c07d344fabc8fdae9c3b9407527dbd8f25e
Ewan88 <baileyewan@gmail.com>
2019-06-24 19:43:07 +0100

    fixes colour tests

--- server/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
+++ server/src/main/java/com/answerdigital/colourstest/controller/ColoursController.java
@@ -3,10 +3,13 @@ package com.answerdigital.colourstest.controller;
 import com.answerdigital.colourstest.model.Colour;
 import com.answerdigital.colourstest.repository.ColoursRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,16 @@ public class ColoursController {
     @GetMapping
     public ResponseEntity<List<Colour>> getColours() {
         return new ResponseEntity(coloursRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Colour> getColour(@PathVariable("id") Long id) {
+        Optional<Colour> colour = coloursRepository.findById(id);
+
+        if (colour.isPresent()) {
+            return new ResponseEntity(colour, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
     }
 }
